8a260ddb86db5bd53cb3572675ff9304b24923f9
adjieevil <adjieevil@gmail.com>
2020-02-08 00:24:55 +0700

    Updates

---
+++ precache-manifest.4740b8b499c0dfb1897cadc3ffb43a1e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aacc269f140cec537eb43cd9cd235c7a",
+    "url": "/widjaaja/index.html"
+  },
+  {
+    "revision": "2c5d7b0458a8d7f0b845",
+    "url": "/widjaaja/static/css/main.456dd4c7.chunk.css"
+  },
+  {
+    "revision": "a7649e4bd5a4d456e708",
+    "url": "/widjaaja/static/js/2.4effeaaa.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/widjaaja/static/js/2.4effeaaa.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2c5d7b0458a8d7f0b845",
+    "url": "/widjaaja/static/js/main.a3d2de69.chunk.js"
+  },
+  {
+    "revision": "60920f2a2755dbeb18f3",
+    "url": "/widjaaja/static/js/runtime-main.a99021d0.js"
+  },
+  {
+    "revision": "2c36d9c420b91b1c29a9524974d7e2cc",
+    "url": "/widjaaja/static/media/tailwindcss.2c36d9c4.svg"
+  }
+]);
