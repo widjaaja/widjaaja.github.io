9a57c0e79e7264e380a143063dc0fb5d2741c83c
adjieevil <adjieevil@gmail.com>
2020-02-08 00:38:51 +0700

    deploy

---
+++ src/App.js
@@ -0,0 +1,117 @@
+import React from 'react';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header mx-auto">
+        <div class="flex flex-col bg-gray-200 my-2">
+          <div class="flex bg-gray-200">
+            <div class="flex-1 text-gray-700 text-center bg-gray-400 px-4">
+              <div class="flex flex-col bg-gray-200">
+                <div class="text-gray-700 font-sans text-center text-7xl leading-tight bg-gray-400 px-4">qwdsadzsczx</div>
+                <div class="text-gray-700 text-center text-sm bg-gray-400 px-4 tracking-widest">Life & Business Coach</div>
+              </div>
+            </div>
+          </div>
+          <div class="flex bg-gray-200 justify-center my-12 relative" style={{height: "75vh"}}>
+            <div class="text-gray-700 text-center bg-gray-400 px-4 py-2 m-2 absolute left-43 inset-y-auto">
+              <div style={{height: "82vh", width: "70vh"}}></div>
+            </div>
+            <div class="flex flex-col bg-gray-200 my-16 absolute left-20 inset-y-auto">
+              <div class="text-gray-700 text-center text-2/3xl tracking-widest bg-gray-400 px-6 py-4 my-2">IT'S TIME TO QUIT</div>
+              <div class="text-gray-700 text-center text-2/3xl tracking-widest bg-gray-400 py-3 my-2">THE TIRING HUSTLE</div>
+              <div class="text-gray-700 text-center text-2/3xl tracking-widest bg-gray-400 py-3 my-2">& BREAK FREE TO</div>
+              <div class="text-gray-700 text-center text-2/3xl tracking-widest bg-gray-400 py-3 my-2">IT'S TIME TO QUIT</div>
+              <div class="text-gray-700 px-4 py-2 my-2 ">
+                <div class="text-sm w-5/12 ">
+                  <span class="">DON'T WORRY... I'VE GOT A PLAN TO SHOW YOU HOW.</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="flex flex-row bg-gray-200 justify-center my-16 mx-40">
+            <div class="text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">
+              <div class="w-64" ></div>
+            </div>
+            <div class="flex flex-col bg-gray-200">
+              <div class="text-gray-700 bg-gray-400 px-4 py-2 my-4">
+                <div class="text-gray-700 text-5xl font-sans leading-none tracking-wider bg-gray-400 ">Hey, I'm wqdsadwas</div>
+                <div class="text-gray-700 text-right tracking-widest text-sm uppercase bg-gray-400">life coach for busy women on a mission</div>
+              </div>
+              <div class="flex flex-row bg-gray-200 text-sm max-w-xl">
+                <div class="text-gray-700 bg-gray-400 px-4 py-2 mx-2">
+                  Swag tbh asymmetrical, activated charcoal schlitz tattooed arcoal schlitz tattooed roof party master cleaLorem ipsum dolor amet yr poke distillery chicharrones green juice gentrify subway tile paleo fingerstache hell of chartreuse brunch. Food truck jianbing gastropub schlitz, hammock wolf chia vice. Keytar freegan biodiesel, bushwick roof party put a bird on it venmo seitan flexitarian. Poke bitters williamsburg, freegan keytar poutine cardigan chia.
+                </div>
+                <div class="text-gray-700 bg-gray-400 px-4 py-2 mx-2">
+                  Microdosing lo-fi everyday carry lyft thundercats. Four loko meh umami pop-up. Migas succulents artisan ennui prism. Fixie sriracha taxidermy brooklyn art party. Occupy small batch leggings paleo bespoke kinfolk yuccie pok pok affogato vinyl put a bird on it. Normcore tattooed tumeric synth, edison bulb tilde semiotics glossier cray fingerstache. Next level salvia shoreditch, master cleanse stumptown fanny pack pitchfork
+                </div>
+              </div>
+              <div class="text-gray-700 text-center bg-gray-400 px-4 py-2 m-2 mt-8">3</div>
+            </div>
+          </div>
+          <div class="flex bg-gray-200 mt-8 mx-auto">
+            <div class="flex flex-col bg-gray-200">
+              <div class="text-gray-700 text-center tracking-widests text-sm bg-gray-400 px-4 py-2 m-2 uppercase">As Technology in </div>
+              <div class="text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">
+                <div class="flex flex-row bg-gray-200">
+                  <div class="text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">1</div>
+                  <div class="text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">2</div>
+                  <div class="text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">3</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="flex bg-gray-400 mt-8 py-4 mx-auto">
+            <div class="flex flex-col bg-gray-200 mx-64">
+              <div class="text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">1</div>
+              <div class="text-gray-700 text-center text-3xl tracking-widests bg-gray-400 px-4 py-2 m-2">"WORKING WITH NICOLE TRULY TRANSFORMED MY LIFE & BUSINESS. I'M FOREVER A FAN!!"</div>
+              <div class="text-gray-700 text-center text-sm tracking-widests bg-gray-400 px-4 ">— CELESTE W</div>
+            </div>
+          </div>
+          <div class="flex flex-col bg-gray-200 my-2">
+              <div class="text-gray-700 text-center bg-gray-400 px-4 py-2">
+                <div class="flex flex-col bg-gray-200 mx-64">
+                  <div class="text-gray-700 text-center text-3xl tracking-widests font-medium bg-gray-400 px-4 py-4">HAVE A HANDSOME HOME</div>
+                  <div class="text-gray-700 text-center bg-gray-400 px-4 py-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim diam nonummy tincidunt ut laoreet dolore</div>
+                  <div class="text-gray-700 text-center bg-gray-400 px-4 py-10">3</div>
+                </div>
+              </div>
+              <div class="text-gray-700 text-center bg-gray-400 px-4 py-2">
+                <div class="flex flex-col bg-gray-200">
+                  <div class="text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">
+                    <div class="flex flex-row bg-gray-200">
+                      <div class="text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">1</div>
+                      <div class="text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">
+                        <div class="flex flex-col bg-gray-200">
+                          <div class="text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">1</div>
+                          <div class="text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">2</div>
+                        </div>
+                      </div>
+                      <div class="text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">
+                        <div class="flex flex-col bg-gray-200">
+                          <div class="text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">1</div>
+                          <div class="text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">2</div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">2</div>
+                  <div class="text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">3</div>
+                </div>
+              </div>
+            </div>
+          <div class="flex bg-gray-600 mb-4 mx-auto">
+            <div class="flex flex-col bg-gray-200 py-8 mx-super px-16 my-6">
+              <div class="text-gray-700 text-center font-medium text-xl tracking-widests bg-gray-400">JOIN THE FUN! LET'S WORK TOGETHER!</div>
+              <div class="text-gray-700 text-center text-sm bg-gray-400 my-2 ">Tote bag vinyl pabst, cronut chia 8-bit su pour-over fanny pack bicycle rights typewriter disrupt.</div>
+              <div class="text-gray-700 text-center bg-gray-400 px-4 my-2">3</div>
+            </div>
+          </div>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
