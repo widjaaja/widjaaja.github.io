99282b127ebeaf91c612f7a5635d0a7fe37f8a2c
adjieevil <adjieevil@gmail.com>
2020-03-12 16:11:16 +0700

    deploy

---
+++ precache-manifest.07227018ee8660920c82b46a56cede01.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7dc0d9167f03c3c701e57eb746a426a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a8ebb03744f10988d7b0",
+    "url": "/static/css/main.0e525978.chunk.css"
+  },
+  {
+    "revision": "12070422b2270de3b456",
+    "url": "/static/js/2.92f15f85.chunk.js"
+  },
+  {
+    "revision": "577ef60f73636c6e95b316a24bc4f83f",
+    "url": "/static/js/2.92f15f85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8ebb03744f10988d7b0",
+    "url": "/static/js/main.206d6a87.chunk.js"
+  },
+  {
+    "revision": "2db7e286640ce4619e33",
+    "url": "/static/js/runtime-main.b5842599.js"
+  },
+  {
+    "revision": "9c5a53003cf967936af3ed37f9323eb5",
+    "url": "/static/media/double-arrow.9c5a5300.svg"
+  },
+  {
+    "revision": "2d2b0d56aeefb1b0f3025195cb2ff7b6",
+    "url": "/static/media/react-logo.2d2b0d56.png"
+  },
+  {
+    "revision": "ccf9b1861318819b5a1290dce2d7a5a9",
+    "url": "/static/media/tailwindcss-logo.ccf9b186.svg"
+  }
+]);
