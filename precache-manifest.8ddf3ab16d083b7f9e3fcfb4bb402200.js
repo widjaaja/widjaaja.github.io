020483c2046e47498903be9ffa008c73b8349046
adjie <adjieevil>
2020-04-10 15:32:44 +0700

    Updates

---
+++ precache-manifest.8ddf3ab16d083b7f9e3fcfb4bb402200.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea58429666fb26b1d70b82390b9cbe10",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0effe0ae5ef66f06b368",
+    "url": "/static/css/main.75ed268d.chunk.css"
+  },
+  {
+    "revision": "b0fa1167442476f80b8d",
+    "url": "/static/js/2.dc5b6788.chunk.js"
+  },
+  {
+    "revision": "bdc7e82db52bf266d5536c19690b95ee",
+    "url": "/static/js/2.dc5b6788.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0effe0ae5ef66f06b368",
+    "url": "/static/js/main.64f44aaf.chunk.js"
+  },
+  {
+    "revision": "37dafb3d304eeaa3738c",
+    "url": "/static/js/runtime-main.7f2b03ba.js"
+  },
+  {
+    "revision": "2b63ff69d79b2a87188e48d0c99d721e",
+    "url": "/static/media/anee.2b63ff69.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/widjaaja.github.io/precache-manifest.98d89a4914e332757f50aa77180447e1.js"
+  "/precache-manifest.8ddf3ab16d083b7f9e3fcfb4bb402200.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/widjaaja.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
