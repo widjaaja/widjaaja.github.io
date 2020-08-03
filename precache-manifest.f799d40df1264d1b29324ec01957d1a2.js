6c13870ef2afab82cf413d396d41626dbc7e8833
adjieevil <adjieevil@gmail.com>
2020-08-03 22:46:27 +0700

    add-project

---
+++ precache-manifest.f799d40df1264d1b29324ec01957d1a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a9598dfdd30facbea80f5fabb647615",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df17423cd9cb90eea707",
+    "url": "/static/css/main.7d4fb22c.chunk.css"
+  },
+  {
+    "revision": "163b7e530b91d344bb59",
+    "url": "/static/js/2.a4914694.chunk.js"
+  },
+  {
+    "revision": "bdc7e82db52bf266d5536c19690b95ee",
+    "url": "/static/js/2.a4914694.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df17423cd9cb90eea707",
+    "url": "/static/js/main.2e3f2d6a.chunk.js"
+  },
+  {
+    "revision": "37dafb3d304eeaa3738c",
+    "url": "/static/js/runtime-main.7f2b03ba.js"
+  },
+  {
+    "revision": "2b63ff69d79b2a87188e48d0c99d721e",
+    "url": "/static/media/anee.2b63ff69.jpg"
+  }
+]);
